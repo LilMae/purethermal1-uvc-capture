7dd6be29f89145785065f74e407e567e725d2a0b
Jeong HoeJun <LilMae@inha.edu>
2024-02-29 16:21:55 +0900

    Update record-uvc-radiometry.py
    Finish record-uvc-radiometry.py

--- python/record-uvc-radiometry.py
+++ python/record-uvc-radiometry.py
@@ -1,19 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from uvctypes import *
-import cv2
-from queue import Queue
+import time
 import numpy as np
+
+try:
+    from queue import Queue
+except ImportError:
+    from Queue import Queue
+import cv2
+import os
 
 BUF_SIZE = 2
 q = Queue(BUF_SIZE)
 
+
 def py_frame_callback(frame, userptr):
 
-    array_pointer = cast(frame.contents.data, POINTER(c_uint16 * (frame.contents.width * frame.contents.height)))
-    data = np.frombuffer(
-        array_pointer.contents, dtype=np.dtype(np.uint16)
-    ).reshape(
-        frame.contents.height, frame.contents.width
+    array_pointer = cast(
+        frame.contents.data,
+        POINTER(c_uint16 * (frame.contents.width * frame.contents.height)),
     )
+    data = np.frombuffer(array_pointer.contents, dtype=np.dtype(np.uint16)).reshape(
+        frame.contents.height, frame.contents.width
+    )  # no copy
+
+    # data = np.fromiter(
+    #   frame.contents.data, dtype=np.dtype(np.uint8), count=frame.contents.data_bytes
+    # ).reshape(
+    #   frame.contents.height, frame.contents.width, 2
+    # ) # copy
 
     if frame.contents.data_bytes != (2 * frame.contents.width * frame.contents.height):
         return
@@ -21,53 +38,103 @@ def py_frame_callback(frame, userptr):
     if not q.full():
         q.put(data)
 
+
 PTR_PY_FRAME_CALLBACK = CFUNCTYPE(None, POINTER(uvc_frame), c_void_p)(py_frame_callback)
 
+
+def ktof(val):
+    return 1.8 * ktoc(val) + 32.0
+
+
+def ktoc(val):
+    return (val - 27315) / 100.0
+
+
+def raw_to_8bit(data):
+    cv2.normalize(data, data, 0, 65535, cv2.NORM_MINMAX)
+    np.right_shift(data, 8, data)
+    return cv2.cvtColor(np.uint8(data), cv2.COLOR_GRAY2RGB)
+
+
 def main():
-    
     ctx = POINTER(uvc_context)()
     dev = POINTER(uvc_device)()
     devh = POINTER(uvc_device_handle)()
     ctrl = uvc_stream_ctrl()
-    
-    res  = libuvc.uvc_init(byref(ctx), 0)
-    
-    # uvc-device 체크
-    if res < 0:
-        print(f'uvc_init error')
-        exit(1)
-    try:
-        res = libuvc.uvc_find_device(ctx, )
-    except:
-        print('uvc_find_device error')
-        exit(1)
-    try:
-        res = libuvc.uvc_open(dev, byref(devh))
-    except:
-        print('uvc_open error')
-        exit(1)
-    
-    # uvc-device 연결 완료
-    print('device opened!')
-    print_device_info(devh)
-    print_device_formats(devh)
-    
-    # uvc-device data 포메팅 확인
-    frame_formats = uvc_get_frame_formats_by_guid(devh, VS_FMT_GUID_Y16)
-    if len(frame_formats) == 0:
-        print('device does not support Y16')
-        exit(1)
-    
-    # 데이터 받아서 열어두기(스트리밍 상태임)
-    res = libuvc.uvc_start_streaming(devh, byref(ctrl), PTR_PY_FRAME_CALLBACK, None, 0)
-    if res < 0:
-        print(f'uvc_start_striming failed : {res}')
-    
-    while True:
-        data = q.get(True,500)
-        if data is None:
-            break
-        print(type(data))
 
-if __name__ == '__main__':
+    res = libuvc.uvc_init(byref(ctx), 0)
+    if res < 0:
+        print("uvc_init error")
+        exit(1)
+
+    try:
+        res = libuvc.uvc_find_device(ctx, byref(dev), PT_USB_VID, PT_USB_PID, 0)
+        if res < 0:
+            print("uvc_find_device error")
+            exit(1)
+
+        try:
+            res = libuvc.uvc_open(dev, byref(devh))
+            if res < 0:
+                print("uvc_open error")
+                exit(1)
+
+            print("device opened!")
+
+            print_device_info(devh)
+            print_device_formats(devh)
+
+            frame_formats = uvc_get_frame_formats_by_guid(devh, VS_FMT_GUID_Y16)
+            if len(frame_formats) == 0:
+                print("device does not support Y16")
+                exit(1)
+
+            libuvc.uvc_get_stream_ctrl_format_size(
+                devh,
+                byref(ctrl),
+                UVC_FRAME_FORMAT_Y16,
+                frame_formats[0].wWidth,
+                frame_formats[0].wHeight,
+                int(1e7 / frame_formats[0].dwDefaultFrameInterval),
+            )
+
+            res = libuvc.uvc_start_streaming(
+                devh, byref(ctrl), PTR_PY_FRAME_CALLBACK, None, 0
+            )
+            if res < 0:
+                print("uvc_start_streaming failed: {0}".format(res))
+                exit(1)
+
+            try:
+                frame_idx = 0
+                tm = time.localtime(time.time())
+
+                folder_path = os.path.join(
+                    os.getcwd(),
+                    f"{tm.tm_mon}_{tm.tm_mday}_{tm.tm_hour}_{tm.tm_min}_{tm.tm_sec}",
+                )
+                os.mkdir(folder_path)
+                while True:
+                    frame_idx += 1
+                    data = q.get(True, 500)
+                    if data is None:
+                        break
+
+                    data = cv2.resize(data[:, :], (640, 480))
+                    minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(data)
+                    img_np = raw_to_8bit(data)
+                    file_name = f"{frame_idx}.npy"
+                    np.save(os.path.join(folder_path, file_name), img_np)
+                    print(f"{frame_idx}th frame is saved")
+            finally:
+                libuvc.uvc_stop_streaming(devh)
+
+            print("done")
+        finally:
+            libuvc.uvc_unref_device(dev)
+    finally:
+        libuvc.uvc_exit(ctx)
+
+
+if __name__ == "__main__":
     main()
